928d7b59f056d71706de782fec71c22e71d6a89b
jackylauu <jackylauu@github.com>
2019-10-18 12:23:09 +0800

    TODO make beautiful templates

--- ppsite/pages/urls.py
+++ ppsite/pages/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', views.user_profile, name='profile'),
+    path('users', views.users, name='users'),
+    path('events', views.events, name='events'),
+    path('ev-profile', views.event_profile, name='ev-profile'),
     #path('admin/', admin.site.urls),
 ]
--- ppsite/pages/views.py
+++ ppsite/pages/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html')
+
+def users(request):
+    return render(request, 'pages/users.html')
+
+def events(request):
+    return render(request, 'pages/events.html')
+
+def user_profile(request):
+    return render(request, 'pages/user_profile.html')
+
+def event_profile(request):
+    return render(request, 'pages/event_profile.html')
+
+
+
+
